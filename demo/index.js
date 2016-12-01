b0287eae52f06cf12629e51d1d4f5bb185fd87b5
zksailor534 <nate.greene@gmail.com>
2016-12-01 15:08:33 -0500

    Add react-router to demo

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './App';
 
 const app = document.getElementsByClassName('demonstration')[0];
 
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+  </Router>,
+  app
+);
