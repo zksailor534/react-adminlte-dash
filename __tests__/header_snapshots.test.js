0f574e52790eef5ea902b84da606322045d99105
zksailor534 <nate.greene@gmail.com>
2017-01-05 11:11:08 -0500

    Header and Navbar/UserMenu component snapshot testing

---
+++ __tests__/header_snapshots.test.js
@@ -0,0 +1,54 @@
+/* eslint-env jest */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// import components
+import Header from '../src/components/Header';
+
+describe('<Header />', () => {
+  it('renders correctly with default options', () => {
+    const component = renderer.create(<Header sidebarToggle={() => null} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with fixed option', () => {
+    const component = renderer.create(<Header fixed sidebarToggle={() => null} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with collapsed option', () => {
+    const component = renderer.create(<Header sidebarCollapse sidebarToggle={() => null} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with fixed and collapsed options', () => {
+    const component = renderer.create(
+      <Header fixed sidebarCollapse sidebarToggle={() => null} />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with given large logo', () => {
+    const component = renderer.create(
+      <Header
+        logoLg={<span>Large</span>}
+        sidebarToggle={() => null}
+      />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with given small logo', () => {
+    const component = renderer.create(
+      <Header
+        logoSm={<span>Small</span>}
+        sidebarToggle={() => null}
+      />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
