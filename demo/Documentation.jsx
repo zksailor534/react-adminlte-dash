d5daab6e5a4cd466c18c1919e584786080d11b1b
zksailor534 <nate.greene@gmail.com>
2017-01-26 11:24:32 -0500

    Improve documentation formatting with skeleton-css

--- demo/Documentation.jsx
+++ demo/Documentation.jsx
@@ -6,11 +6,11 @@ import './Documentation.css';
 const readme = require('../README.md');
 
 const Documentation = () => (
-  <div>
-    <section>
+  <div className="skeleton" style={{ paddingTop: '25px' }}>
+    <section className="container">
       <h1>Documentation</h1>
     </section>
-    <section>
+    <section className="container">
       <Markdown source={readme} />
     </section>
   </div>
